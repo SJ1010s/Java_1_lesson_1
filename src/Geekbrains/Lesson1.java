94c3e807b74d417157a214e3f5895b0ba6fb8384
Evgenii <evgenii2451010@gmail.com>
2020-09-25 19:02:29 +0700

    lesson1_commit

--- src/Geekbrains/Lesson1.java
+++ src/Geekbrains/Lesson1.java
@@ -6,7 +6,7 @@ public class Lesson1 {
 
         byte a;
         a = 120;
-        System.out.println(a)
+        System.out.println(a);
         short b = 11240;
         System.out.println(b);
         long c = 124562L;
@@ -17,44 +17,44 @@ public class Lesson1 {
         System.out.println(e);
 
 
-//        System.out.println("a*(b+(c/d)) = " + expression(4, 3, 5, 6));
-//        summ(4, 6);
-//        plusMinus(0);
-//        System.out.println(plusMinus2(-1));
-//        printHello("Евгений");
-//    }
-//
-//    static float expression(float a, float b, float c, float d) {
-//        return a * (b + (c / d));
-//
-//    }
-//
-//    static void summ(int a, int b) {
-//        int s = a + b;
-//        int c = 10;
-//        int d = 20;
-//        if (s >= c && s <= d) {
-//            System.out.println("Сумма лежит в пределах от 10 до 20 включительно");
-//        } else
-//            System.out.println("Сумма выходит за пределы от 10 до 20");
-//    }
-//
-//    static void plusMinus(int a) {
-//        if (a < 0) {
-//            System.out.println("Число отрицательное");
-//        } else {
-//            System.out.println("Число положительное");
-//        }
-//    }
-//    static boolean plusMinus2(int a){
-//        if (a<0) {
-//            return true;
-//        } else{
-//            return false;
-//        }
-//    }
-//    static void printHello(String txtHello){
-//        System.out.println("Привет, "+txtHello);
-//    }
+        System.out.println("a*(b+(c/d)) = " + expression(4, 3, 5, 6));
+        summ(4, 6);
+        plusMinus(0);
+        System.out.println(plusMinus2(-1));
+        printHello("Евгений");
+    }
+
+    static float expression(float a, float b, float c, float d) {
+        return a * (b + (c / d));
+
+    }
+
+    static void summ(int a, int b) {
+        int s = a + b;
+        int c = 10;
+        int d = 20;
+        if (s >= c && s <= d) {
+            System.out.println("Сумма лежит в пределах от 10 до 20 включительно");
+        } else
+            System.out.println("Сумма выходит за пределы от 10 до 20");
+    }
+
+    static void plusMinus(int a) {
+        if (a < 0) {
+            System.out.println("Число отрицательное");
+        } else {
+            System.out.println("Число положительное");
+        }
+    }
+    static boolean plusMinus2(int a){
+        if (a<0) {
+            return true;
+        } else{
+            return false;
+        }
+    }
+    static void printHello(String txtHello){
+        System.out.println("Привет, "+txtHello);
+    }
 
 }
