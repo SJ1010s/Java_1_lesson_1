4a5f8aceaeb346ee1e926860eb436b53cccbd5b4
Evgenii <evgenii2451010@gmail.com>
2020-11-22 15:46:38 +0700

    Домашняя работа 2-5.2

---
+++ src/Lesson_2_5/Main.java
@@ -0,0 +1,10 @@
+package Lesson_2_5;
+
+public class Main {
+    public static void main(String[] args) {
+        MyArrayClass array_1 = new MyArrayClass();
+        MyArrayClass array_2 = new MyArrayClass();
+        array_1.myArray1();
+        array_2.myArray2();
+    }
+}
